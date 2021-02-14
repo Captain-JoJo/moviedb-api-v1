dea8fd94fbdc69e93728461be15c4ea6dfac14c6
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-14 16:46:20 -0600

    updated to add call from postman

--- src/db/config/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/inventoryDB'
-}
---
+++ src/models/movie-model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Movie = new Schema(
+    {
+        name: { type: String, required: false },
+        rating: { type: Number, required: false },
+    }
+)
+
+module.exports = mongoose.model('movies', Movie)
