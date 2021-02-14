5e1ac21734ff0f6ee598df0dae298c27ac160152
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-14 16:46:41 -0600

    updated to add call from postman

--- src/models/movie-model.js
+++ src/models/movie-model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const Movie = new Schema(
     {
-        name: { type: String, required: false },
+        name: { type: String, required: true },
         rating: { type: Number, required: false },
     }
 )
